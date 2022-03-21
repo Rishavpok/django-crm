60b3eda612fde107f775cb8482be066f366259af
rishav <rishav.pokharel@newitventure.com>
2022-03-21 15:09:33 +0545

    first push

---
+++ accounts/models.py
@@ -0,0 +1,55 @@
+import email
+from unicodedata import category
+from django.db import models
+
+from accounts.views import customer
+
+# Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=11 , null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True , null=True)
+    
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out door', 'Out door')
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True , blank=True)
+    date_created = models.DateTimeField(auto_now_add=True , null=True)
+    tags=  models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivired', 'Delivired')
+    )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True , null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+import imp
+from operator import imod
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('product/', views.product),
+    path('customer/', views.customer),
+]
